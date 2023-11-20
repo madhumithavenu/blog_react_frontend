174d85248941413cf8e2475fec7cc253cae45e18
Madhumitha <madhumithavenu03@gmail.com>
2023-11-20 10:29:45 +0530

    Adding header contents

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,19 @@
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <AppBar sx={{ background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(48,9,121,1) 35%, rgba(0,212,255,1) 100%)' }}>
+      <Toolbar>
+        <Typography variant='h4'>
+          Blog App
+        </Typography>
+        <Box display={"flex"} marginLeft={"auto"}>
+          <Button variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Login</Button>
+          <Button variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Signup</Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
   )
 }
 
