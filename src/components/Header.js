3f0e88242ebb7286d941e2d4ee0addf8ae0cec02
Madhumitha <madhumithavenu03@gmail.com>
2023-11-20 11:36:48 +0530

    Adding login function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,26 @@
-import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material'
-import React from 'react'
+import { AppBar, Box, Button, Tab, Tabs, Toolbar, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
+  const [value, setValue] = useState();
+
   return (
-    <AppBar sx={{ background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(48,9,121,1) 35%, rgba(0,212,255,1) 100%)' }}>
+    <AppBar position='sticky' sx={{ background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,19,121,1) 0%, rgba(0,212,255,1) 100%)' }}>
       <Toolbar>
         <Typography variant='h4'>
           Blog App
         </Typography>
+        {props.isLoggedIn && <Box display={"flex"} marginLeft={'auto'} marginRight={"auto"}>
+          <Tabs textColor='inherite' value={value} onChange={(e, val) => setValue(val)}>
+            <Tab LinkComponent={Link} to="/blogs" label="All Blogs" />
+            <Tab LinkComponent={Link} to="/myBlogs" label="My Blogs" />
+          </Tabs>
+        </Box>}
         <Box display={"flex"} marginLeft={"auto"}>
-          <Button variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Login</Button>
-          <Button variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Signup</Button>
+          <Button LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Login</Button>
+          <Button LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Signup</Button>
+          {props.isLoggedIn && <Button LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Logout</Button>}
         </Box>
       </Toolbar>
     </AppBar>
