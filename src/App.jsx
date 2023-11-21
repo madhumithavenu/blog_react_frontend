91004f4ef179b58a5fb38f7b1b780fd151df511f
Madhumitha <madhumithavenu03@gmail.com>
2023-11-21 14:51:23 +0530

    Creating a navbar and setting up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Header from './components/Header.jsx'
+import { Route, Routes } from 'react-router-dom'
+import BlogDetail from './components/BlogDetail.jsx'
+import UserBlogs from './components/UserBlogs.jsx'
+import AddBlog from './components/AddBlog.jsx'
+import Auth from './components/Auth.jsx'
+import Blogs from './components/Blogs.jsx'
 
 function App() {
   return (
-    <div>Hello World</div>
+    <>
+    <header>
+    <Header/>
+    </header>
+    <main>
+      <Routes>
+        <Route path='/auth' element={<Auth/>}/>
+        <Route path='/blogs' element={<Blogs/>}/>
+        <Route path='/myBlogs' element={<UserBlogs/>}/>
+        <Route path='/myBlogs/:id' element={<BlogDetail/>}/>
+        <Route path='/blogs/add' element={<AddBlog/>}/>
+      </Routes>
+    </main>
+    </>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <AppBar position='sticky' sx={{ background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,19,121,1) 0%, rgba(0,212,255,1) 100%)' }}>
+      <Toolbar>
+        <Typography variant='h4'>Blog App</Typography>
+        <Box display={'flex'} marginLeft={'auto'}>
+          <Button variant='contained' sx={{margin:1, borderRadius:10}} color='warning'>Login</Button>
+          <Button variant='contained' sx={{margin:1, borderRadius:10}} color='warning'>Signup</Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
   )
 }
 
