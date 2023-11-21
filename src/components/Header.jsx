e5d61ea5731bc4e89e8459412dbafa73e0996482
Madhumitha <madhumithavenu03@gmail.com>
2023-11-21 14:54:42 +0530

    Adding label

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material'
+import { AppBar, Box, Button, Tab, Tabs, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 
 function Header() {
@@ -6,6 +6,12 @@ function Header() {
     <AppBar position='sticky' sx={{ background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,19,121,1) 0%, rgba(0,212,255,1) 100%)' }}>
       <Toolbar>
         <Typography variant='h4'>Blog App</Typography>
+        <Box display={'flex'}>
+          <Tabs>
+            <Tab label="All Blogs"/>
+            <Tab label="My Blogs"/>
+          </Tabs>
+        </Box>
         <Box display={'flex'} marginLeft={'auto'}>
           <Button variant='contained' sx={{margin:1, borderRadius:10}} color='warning'>Login</Button>
           <Button variant='contained' sx={{margin:1, borderRadius:10}} color='warning'>Signup</Button>
