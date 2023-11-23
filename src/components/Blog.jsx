3324def71491b58f6275b632aca7beeb3f4c61d8
Madhumitha <madhumithavenu03@gmail.com>
2023-11-23 09:14:10 +0530

    adding blog card

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { Avatar, Card, CardContent, CardHeader, CardMedia, Typography } from '@mui/material'
+import React from 'react';
 
 function Blog() {
   return (
-    <div>Blog</div>
+    <div>
+      <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: 'red' }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image="/static/images/cards/paella.jpg"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the mussels,
+          if you like.
+        </Typography>
+      </CardContent>
+    </Card>
+    </div>
   )
 }
 
--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,19 +1,24 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import Blog from './Blog';
 
 function Blogs() {
- const [blogs, setBlogs] = useState([]);
-    async function sendRequest (){
-      const res = await axios.get("http://localhost:5000/api/blog")
-      .catch(err=>console.log(err));
-      const data = await res.data;
-      return data;
-    };
-    useEffect(()=>{
-      sendRequest().then((data)=>console.log(data)); 
-  },[]);
+  const [blogs, setBlogs] = useState([]);
+  async function sendRequest() {
+    const res = await axios.get("http://localhost:5000/api/blog")
+      .catch(err => console.log(err));
+    const data = await res.data;
+    return data;
+  };
+  useEffect(() => {
+    sendRequest().then((data) => console.log(data.blogs));
+  }, []);
   return (
-    <div>Blogs</div>
+    <div>
+      <Blog />
+      <Blog />
+      <Blog />
+      </div>
   )
 }
 
